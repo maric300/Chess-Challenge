e9e6ddb7d9e1ea9a49318273ac43fc14491165f7
maric300 <branko.maric300@gmail.com>
2023-07-23 03:39:42 +0200

    feat: centralization evaluation function added
    Bot starts moving the same piece after a while

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,23 +2,24 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-    int depth = 5;
+    int depth = 4;
+    string[] centerSquares = { "d4", "d5", "e4", "e5"};
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
         Move moveToPlay = moves[0];
         int minValue = 100000;
 
-
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int value = Max(board, depth, -100000, 100000);
+            int value = Max(board, depth, -500000, 500000);
             if (value < minValue)
             {
                 minValue = value;
@@ -35,9 +36,9 @@ public class MyBot : IChessBot
         depth -= 1;
         int maxValue = -100000;
 
-        if (depth < 1)
+        if (depth < 1 || board.IsInCheckmate())
         {
-            return Evaluate(board);
+            return Evaluate(board, true);
         }
 
         foreach (Move move in moves)
@@ -64,38 +65,14 @@ public class MyBot : IChessBot
         return maxValue;
     }
 
-    private int Evaluate(Board board)
-    {
-
-        
-        
-        int whitePieces = 0;
-        whitePieces += board.GetPieceList(PieceType.Pawn, true).Count * 100;
-        whitePieces += board.GetPieceList(PieceType.Knight, true).Count * 300;
-        whitePieces += board.GetPieceList(PieceType.Bishop, true).Count * 300;
-        whitePieces += board.GetPieceList(PieceType.Rook, true).Count * 500;
-        whitePieces += board.GetPieceList(PieceType.Queen, true).Count * 900;
-
-        int blackPieces = 0;
-        blackPieces += board.GetPieceList(PieceType.Pawn, false).Count * 100;
-        blackPieces += board.GetPieceList(PieceType.Knight, false).Count * 300;
-        blackPieces += board.GetPieceList(PieceType.Bishop, false).Count * 300;
-        blackPieces += board.GetPieceList(PieceType.Rook, false).Count * 500;
-        blackPieces += board.GetPieceList(PieceType.Queen, false).Count * 900;
-
-
-
-        return whitePieces - blackPieces;
-    }
-
     private int Min(Board board, int depth, int alpha, int beta)
     {
         Move[] moves = board.GetLegalMoves();
         depth -= 1;
         int minValue = 100000;
-        if (depth < 1)
+        if (depth < 1 || board.IsInCheckmate())
         {
-            return Evaluate(board);
+            return Evaluate(board, false);
         }
         foreach (Move move in moves)
         {
@@ -119,5 +96,54 @@ public class MyBot : IChessBot
             }
         }
         return minValue;
+    }
+
+    private int Evaluate(Board board, bool whiteToMove)
+    {
+        if (board.IsInCheckmate()) {
+            return (whiteToMove) ? -1111111 : 1111111;
+        }
+        int evaluation = EvaluateMaterial(board);
+        evaluation += EvaluateCentralization(board, whiteToMove);
+        return evaluation;
+    }
+
+    private int EvaluateMaterial(Board board)
+    {
+        int whitePieces = 0;
+        whitePieces += board.GetPieceList(PieceType.Pawn, true).Count * 100;
+        whitePieces += board.GetPieceList(PieceType.Knight, true).Count * 300;
+        whitePieces += board.GetPieceList(PieceType.Bishop, true).Count * 300;
+        whitePieces += board.GetPieceList(PieceType.Rook, true).Count * 500;
+        whitePieces += board.GetPieceList(PieceType.Queen, true).Count * 900;
+
+        int blackPieces = 0;
+        blackPieces += board.GetPieceList(PieceType.Pawn, false).Count * 100;
+        blackPieces += board.GetPieceList(PieceType.Knight, false).Count * 300;
+        blackPieces += board.GetPieceList(PieceType.Bishop, false).Count * 300;
+        blackPieces += board.GetPieceList(PieceType.Rook, false).Count * 500;
+        blackPieces += board.GetPieceList(PieceType.Queen, false).Count * 900;
+
+        return whitePieces - blackPieces;
+    }
+
+    private int EvaluateCentralization(Board board, bool whiteToMove)
+    {
+        int evaluation = 0;
+        int centerPoints = 250;
+        foreach(Move move in board.GetLegalMoves())
+        {
+            if (centerSquares.Contains(move.TargetSquare.Name)) {
+                if (board.GetPiece(move.StartSquare).IsWhite)
+                {
+                    evaluation += centerPoints;
+                }
+                else
+                {
+                    evaluation -= centerPoints;
+                }
+            }
+        }
+        return evaluation;
     }
 }
